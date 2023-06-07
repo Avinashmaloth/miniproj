400c69e89b6dd01d81d758bccbf7199ebd8350a2
unknown <Avinashmaloth>
2023-06-08 04:57:16 +0530

    initial commit

---
+++ url.js
@@ -0,0 +1,3 @@
+//module.exports = `mongodb://0.0.0.0:27017`
+module.exports = `mongodb+srv://admin:admin@cluster0.vanxhye.mongodb.net/`
+module.exports = `https://github.com/Avinashmaloth/miniproj.git`
